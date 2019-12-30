c88576e7819f0ecc8f9eb86b4cef3617185fa256
Dervişhan <dervishan.sezer@gmail.com>
2019-12-30 22:21:38 +0300

    ui started
    .

--- ELESLMS/UI/LoginWindow.xaml.cs
+++ ELESLMS/UI/LoginWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ELESLMS
+namespace ELESLMS.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// LoginWindow.xaml etkileşim mantığı
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
